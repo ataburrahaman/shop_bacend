a61a9c2d62b7798b77945fcc6cddbeeb0df06f85
300068803_atabur <atabur.rahaman@myntra.com>
2020-06-25 08:09:53 +0530

    conflit checking

--- start.js
+++ start.js
@@ -1,4 +1,3 @@
-
 let express = require('express');
 // Import Body parser
 let bodyParser = require('body-parser');
@@ -11,36 +10,36 @@ let app = express();
 let apiRoutes = require("./routes/index");
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(bodyParser.json());
-let uri ="mongodb://127.0.0.1:27017/groceryshop"
+let uri = "mongodb://127.0.0.1:27017/groceryshop"
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
+//Hare are the conflite
 // Connect to Mongoose and set connection variable
 try {
-    mongoose.connect(uri, options);
+  mongoose.connect(uri, options);
   //  mongoose.connect('', { useNewUrlParser: true});
 } catch (error) {
-    handleError(error);
-  }
-    var db = mongoose.connection;
+  handleError(error);
+}
+var db = mongoose.connection;
 
 // Added check for DB connection
-if(!db)
-    console.log("Error connecting db")
+if (!db)
+  console.log("Error connecting db")
 else
-    console.log("Db connected successfully")
+  console.log("Db connected successfully")
 
 // Setup server port
 var port = process.env.PORT || 8080;
@@ -52,5 +51,5 @@ var port = process.env.PORT || 8080;
 app.use('/shop/api', apiRoutes);
 // Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running groceryshop on port " + port);
+  console.log("Running groceryshop on port " + port);
 });
